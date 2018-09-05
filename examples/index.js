12a524c0bffb4ef97699639261db6c7d7ad17e6e
Leonardo Uribe <luribeto@gmail.com>
2018-09-05 18:19:43 -0500

    example 1

---
+++ examples/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const db = require('../')
+
+async function run () {
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'leo',
+    password: process.env.DB_PASS || 'nano.nano',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres'
+  }
+
+  const { Agent, Metric } = await db(config).catch(handleFatalError)
+
+  const agent = await Agent.createOrUpdate({
+    uuid: 'yyy',
+    name: 'test',
+    username: 'test',
+    hostname: 'test',
+    pid: 1,
+    connected: true
+  }).catch(handleFatalError)
+
+  console.log('--agent--')
+  console.log(agent)
+  
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+run()
